55d3eb84f294ce09beefb390b79ac4195bfe34ca
PaulQuiDev <80120197+PaulQuiDev@users.noreply.github.com>
2024-12-23 11:29:34 +0100

    add work

---
+++ index.js
@@ -0,0 +1,227 @@
+import * as webllm from "https://esm.run/@mlc-ai/web-llm";
+
+/*************** WebLLM logic ***************/
+const messages = [
+  {
+    content: "You are a helpful AI agent helping users.",
+    role: "system",
+  },
+];
+
+const availableModels = webllm.prebuiltAppConfig.model_list.map(
+  (m) => m.model_id,
+);
+let selectedModel = "TinyLlama-1.1B-Chat-v0.4-q4f16_1-MLC";
+
+// Callback function for initializing progress
+function updateEngineInitProgressCallback(report) {
+  console.log("initialize", report.progress);
+  document.getElementById("download-status").textContent = report.text;
+}
+
+// Create engine instance
+const engine = new webllm.MLCEngine();
+engine.setInitProgressCallback(updateEngineInitProgressCallback);
+
+async function initializeWebLLMEngine() {
+  document.getElementById("download-status").classList.remove("hidden");
+  selectedModel = document.getElementById("model-selection").value;
+  const config = {
+    temperature: 1.0,
+    top_p: 1,
+  };
+  await engine.reload(selectedModel, config);
+
+  // Show genre selection step
+  document.querySelector(".genre-container").classList.remove("hidden");
+  document.getElementById("download-status").classList.add("hidden");
+}
+
+async function streamingGenerating(messages, onUpdate, onFinish, onError) {
+  try {
+    let curMessage = "";
+    let usage;
+    const completion = await engine.chat.completions.create({
+      stream: true,
+      messages,
+      stream_options: { include_usage: true },
+    });
+    for await (const chunk of completion) {
+      const curDelta = chunk.choices[0]?.delta.content;
+      if (curDelta) {
+        curMessage += curDelta;
+      }
+      if (chunk.usage) {
+        usage = chunk.usage;
+      }
+      onUpdate(curMessage);
+    }
+    const finalMessage = await engine.getMessage();
+    onFinish(finalMessage, usage);
+  } catch (err) {
+    onError(err);
+  }
+}
+
+async function strGenerating(messages, onFinish, onError) {
+  try {
+    let curMessage = "";
+    let usage;
+    const completion = await engine.chat.completions.create({
+      stream: true,
+      messages,
+      stream_options: { include_usage: true },
+    });
+    for await (const chunk of completion) {
+      const curDelta = chunk.choices[0]?.delta.content;
+      if (curDelta) {
+        curMessage += curDelta;
+      }
+      if (chunk.usage) {
+        usage = chunk.usage;
+      }
+      
+    }
+    const finalMessage = await engine.getMessage();
+    onFinish(finalMessage, usage);
+  } catch (err) {
+    onError(err);
+  }
+}
+
+/*************** UI logic ***************/
+function onMessageSend() {
+  const input = document.getElementById("user-input").value.trim();
+  const message = {
+    content: input,
+    role: "user",
+  };
+  if (input.length === 0) {
+    return;
+  }
+  
+
+  messages.push(message);
+  appendMessage(message);
+
+  document.getElementById("user-input").value = "";
+  document
+    .getElementById("user-input")
+    .setAttribute("placeholder", "Generating...");
+
+  const aiMessage = {
+    content: "typing...",
+    role: "assistant",
+  };
+  appendMessage(aiMessage);
+
+  const onFinishGenerating = (finalMessage, usage) => {
+    updateLastMessage(finalMessage);
+    
+    const usageText =
+      `prompt_tokens: ${usage.prompt_tokens}, ` +
+      `completion_tokens: ${usage.completion_tokens}, ` +
+      `prefill: ${usage.extra.prefill_tokens_per_s.toFixed(4)} tokens/sec, ` +
+      `decoding: ${usage.extra.decode_tokens_per_s.toFixed(4)} tokens/sec`;
+    document.getElementById("chat-stats").classList.remove("hidden");
+    document.getElementById("chat-stats").textContent = usageText;
+  };
+
+  streamingGenerating(
+    messages,
+    updateLastMessage,
+    onFinishGenerating,
+    console.error,
+  );
+}
+
+function appendMessage(message) {
+  const chatBox = document.getElementById("chat-box");
+  const container = document.createElement("div");
+  container.classList.add("message-container");
+  const newMessage = document.createElement("div");
+  newMessage.classList.add("message");
+  newMessage.textContent = message.content;
+
+  if (message.role === "user") {
+    container.classList.add("user");
+  } else {
+    container.classList.add("assistant");
+  }
+
+  container.appendChild(newMessage);
+  chatBox.appendChild(container);
+  chatBox.scrollTop = chatBox.scrollHeight; // Scroll to the latest message
+}
+
+function updateLastMessage(content) {
+  const messageDoms = document
+    .getElementById("chat-box")
+    .querySelectorAll(".message");
+  const lastMessageDom = messageDoms[messageDoms.length - 1];
+  lastMessageDom.textContent = content;
+}
+
+
+function Request(texte) {
+  const input = texte;
+  const message = {
+    content: input,
+    role: "user",
+  };
+  if (input.length === 0) {
+    return;
+  }
+  
+
+  messages.push(message);
+  
+  const onFinishGenerating = (finalMessage, usage) => {
+    return finalMessage
+  };
+
+  strGenerating(
+    messages,
+    onFinishGenerating,
+    console.error,
+  );
+}
+
+/*************** UI binding ***************/
+availableModels.forEach((modelId) => {
+  const option = document.createElement("option");
+  option.value = modelId;
+  option.textContent = modelId;
+  document.getElementById("model-selection").appendChild(option);
+});
+document.getElementById("model-selection").value = selectedModel;
+
+document.getElementById("download").addEventListener("click", function () {
+  initializeWebLLMEngine().then(() => {
+    //document.getElementById("send").disabled = false;
+  });
+});
+
+document.getElementById("start-story").addEventListener("click", function () {
+  //Ui bloque 
+  document.getElementById("download").style.display = "none";
+  document.getElementById("model-selection").disabled = true;
+  
+  // Show story step after genre selection
+  document.querySelector(".genre-container").classList.add("hidden");
+  document.querySelector(".story-container").classList.remove("hidden");
+
+  const genreSelect = document.getElementById("genre-selection").value;
+  console.log(genreSelect);
+  if(genreSelect === "fantasy"){
+    document.getElementById("user-input").value = "tell me a fantasy story";
+    }
+  onMessageSend();
+});
+
+//document.getElementById("send").addEventListener("click", function () {
+//  onMessageSend();
+// });
+
+/********init exanchange********/
+
